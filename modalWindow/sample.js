671b8c16090297b9c18baa58f61585630cdb0a28
“halmuda” <halmuda6231@gmail.com>
2020-03-07 08:18:45 +0900

    add: modalウィンドウを作成

---
+++ modalWindow/sample.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const btn = document.querySelector('#btn');
+  const modal = document.querySelector('#modal');
+  const closeBtn = document.querySelector('#closeBtn');
+
+  closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+  })
+  btn.addEventListener('click', () => {
+    modal.style.display = 'block';
+  })
+}, false);
